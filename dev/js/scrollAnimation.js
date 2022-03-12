9a2c5f6519a5ffb4c3caf469e511199475b07792
JulesSC <81724365+JulesSC@users.noreply.github.com>
2022-03-12 11:41:08 -0500

    scroll trigger set up

--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -4,76 +4,104 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export function skillsTrigger(){
+  
+  // gsap.from(".left-1-text", {
+  //   scrollTrigger:{
+  //     trigger: ".left-1-text",
+  //     start: "top bottom",
+  //     end: "bottom bottom",
+  //     toggleActions: "restart none resume none",
+  //     markers: true,
+  //     scrub: true
+  //   }, 
+  //   duration:0.5,
+  //   y:-65,
+  //   ease: "power1.out",
+  //   stagger: 0.05
+  // })
 
-    // var rotate = gsap.timeline({
-    //     scrollTrigger:{
-    //       trigger: ".ms-section",
-    //       scrub:0.2,
-    //       start: 'top top',
-    //       end:'bottom bottom',
-    //       markers: true
-    //     }
-    //   })
-    //   .to('#img-container', {
-    //     rotation:360*5,
-    //     duration:1, ease:'none'
-    //   })
+  gsap.from(".left-2-text", {
+    scrollTrigger:{
+      trigger: ".left-2-text",
+      start: "top center",
+      end: "bottom bottom",
+      toggleActions: "restart none resume none",
+      markers: true,
+      scrub: true
+    },
+    duration:0.5,
+    y:-65,
+    ease: "power1.out",
+    stagger: 0.05
+  })
 
-    // const tl = new gsap.timeline();
+//   gsap.from(".left-3-text", {
+//     scrollTrigger:{
+//       trigger: ".left-3-text",
+//       start: "top center",
+//       toggleActions: "restart none resume none",
+//       markers: true,
+//       scrub: true
+//     },
+//     duration:0.5,
+//     y:-65,
+//     ease: "power1.out",
+//     stagger: 0.05
+//   })
 
-    // tl.to("#img-container", {
-    //     duration: 1, 
-    //     scaleX: 2, 
-    //     scrollTrigger: {
-    //         trigger: ".ms-section",
-    //         start: "top bottom",
-    //         end: "bottom top",
-    //         toggleActions: "restart none resume none",
-    //         scrub: true,
-    //         markers: true
-    //     }
-    // });
+//   gsap.from(".left-4-text", {
+//     scrollTrigger:{
+//       trigger: ".left-4-text",
+//       start: "top center",
+//       toggleActions: "restart none resume none",
+//       markers: true,
+//       scrub: true
+//     },
+//     duration:0.5,
+//     y:-65,
+//     ease: "power1.out",
+//     stagger: 0.05
+//   })
 
-    // const tl = gsap.timeline({
-    //     scrollTrigger: {
-    //       trigger: ".starring",
-    //       start: "center top",
-    //       end: "center bottom",
-    //       scrub: true,
-    //       markers: true
-    //     }
-    //   });
-    //   tl.to("#img", {rotate: 180, duration: 3})
+//   gsap.from(".left-5-text", {
+//     scrollTrigger:{
+//       trigger: ".left-5-text",
+//       start: "top center",
+//       toggleActions: "restart none resume none",
+//       markers: true,
+//       scrub: true
+//     },
+//     duration:0.5,
+//     y:-65,
+//     ease: "power1.out",
+//     stagger: 0.05
+//   })
 
-      let sections = gsap.utils.toArray(".ms-section"),
-    currentSection = sections[0];
+//   gsap.from(".left-6-text", {
+//     scrollTrigger:{
+//       trigger: ".left-6-text",
+//       start: "top center",
+//       toggleActions: "restart none resume none",
+//       markers: true,
+//       scrub: true
+//     },
+//     duration:0.5,
+//     y:-65,
+//     ease: "power1.out",
+//     stagger: 0.05
+//   })
 
-    gsap.defaults({overwrite: 'auto', duration: 0.3});
-
-    // create a ScrollTrigger for each section
-    sections.forEach((section, i) => {
-      ScrollTrigger.create({
-        // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
-        start: () => (i - 0.5) * innerHeight,
-        end: () => (i + 0.5) * innerHeight,
-        // when a new section activates (from either direction), set the section accordinglyl.
-        onToggle: self => self.isActive && setSection(section)
-      });
-    });
-
-    function setSection(newSection) {
-      if (newSection !== currentSection) {
-        gsap.to(currentSection, {scale: 0.8, autoAlpha: 0})
-        gsap.to(newSection, {scale: 1, autoAlpha: 1});
-        currentSection = newSection;
-      }
-    }
-
-    // handles the infinite part, wrapping around at either end....
-    // ScrollTrigger.create({
-    //   start: 1,
-    //   end: () => ScrollTrigger.maxScroll(window) - 1,
-    //   onLeaveBack: self => self.scroll(ScrollTrigger.maxScroll(window) - 2),
-    //   onLeave: self => self.scroll(2)
-    // }).scroll(2);
+//   gsap.from(".left-7-text", {
+//     scrollTrigger:{
+//       trigger: ".left-7-text",
+//       start: "top center",
+//       toggleActions: "restart none resume none",
+//       markers: true,
+//       scrub: true
+//     },
+//     duration:0.5,
+//     y:-65,
+//     ease: "power1.out",
+//     stagger: 0.05
+//   })
 }
